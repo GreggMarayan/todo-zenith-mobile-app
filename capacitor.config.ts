4dd58e9f53b420534e332e0cfe3df9ca2caeb6cd
gronk <gregg.marayan@gmail.com>
2025-05-28 19:23:41 +0800

    FIXED: TAILWIND NO WORKING

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,12 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.2252e67b66884586bcff9a8fd1d3e50b',
-  appName: 'todo-zenith-mobile-app',
+  appId: 'com.todozenith.mobile',
+  appName: 'Todo Zenith',
   webDir: 'dist',
   server: {
-    cleartext: true,
-    androidScheme: "https",
-    allowNavigation: ["todo-list.dcism.org"]
-  },
+    androidScheme: 'https'
+  }
 };
 
 export default config;
